77f99c017e794628e6d97ef23974273a93f40954
majestiq <majestiq@e6a8e95e-888d-11de-a4f2-b7219ab9ea91>
2009-10-28 18:31:56 +0000

    update

--- app/assistants/azan-popup-assistant.js
+++ app/assistants/azan-popup-assistant.js
@@ -18,11 +18,15 @@ AzanPopupAssistant.prototype.setup = function() {
 	
 	this.controller.get('message').update("Time for " + this.prayer);
 
+	
+
+
 }
 
 AzanPopupAssistant.prototype.activate = function(event) {
 	/* put in event handlers here that should only be in effect when this scene is active. For
 	   example, key handlers that are observing the document */
+
 }
 
 
